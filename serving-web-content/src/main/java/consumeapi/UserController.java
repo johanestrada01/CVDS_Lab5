ae4ed9c927aa2652a460d942c5c15c275483438c
anndr0 <ana.duran-b@mail.escuelaing.edu.co>
2024-02-23 21:38:31 -0500

    part 4

---
+++ serving-web-content/src/main/java/consumeapi/UserController.java
@@ -0,0 +1,35 @@
+package consumeapi;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class UserController {
+
+    @RequestMapping("/hello")
+    private String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/user/{id}")
+    private ModelAndView getUser(@PathVariable Integer id, Model model)   {
+        String uri = "https://jsonplaceholder.typicode.com/users/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+
+        User user = restTemplate.getForObject(uri, User.class);
+        Address address = user.getAddress();
+        Geo geo = address.getGeo();
+        Company company = user.getCompany();
+
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("address", address);
+        modelAndView.addObject("geo", geo);
+        modelAndView.addObject("company", company);
+
+        return modelAndView;
+    }
+
+}
